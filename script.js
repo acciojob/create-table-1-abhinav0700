16d804c954cb8dd2684e23f6a7275aed2718f6e8
Abhinav M <130998983+abhinav0700@users.noreply.github.com>
2025-10-17 14:53:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function insert_Row() {
     let table = document.getElementById("sampleTable");
-    let row = table.insertRow(-1); // Insert at the end
+    let row = table.insertRow(0); // Insert at the end
     let cell1 = row.insertCell(0);
     let cell2 = row.insertCell(1);
     cell1.innerHTML = "New Cell1";
